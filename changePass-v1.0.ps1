386f62d162c3647ee5093d21d2bce68e77659a7f
efo1291 <44806604+efo1291@users.noreply.github.com>
2020-06-18 15:24:10 -0300

    Update changePass-v1.0.ps1

--- changePass-v1.0.ps1
+++ changePass-v1.0.ps1
@@ -1,5 +1,5 @@
 #v1.0
 $User = 'lab1'
 $Pass = ConvertTo-SecureString "L@b12345" –AsPlainText -Force
-$objectid = (Get-AzureADUser -Filter "userPrincipalName eq '$User@cin.ufpe.br'").objectid
+$objectid = (Get-AzureADUser -Filter "userPrincipalName eq '$User@contoso.com'").objectid
 Set-AzureADUserPassword -ObjectId $objectid -Password $Pass -ForceChangePasswordNextLogin $true
